47a6f78a4d6574df59d653111145c8bedcd90cc2
mDevlopers <mathias.tss2012@gmail.com>
2025-10-05 13:53:46 +0200

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -61,7 +61,7 @@ export default function Page() {
         <AuthForm action={handleSubmit} defaultEmail={email}>
           <SubmitButton isSuccessful={isSuccessful}>Se connecter</SubmitButton>
           <p className="text-center text-sm text-gray-600 mt-4 dark:text-zinc-400">
-            Vous n&apos;avez pas de compte ?
+            Vous n&apos;avez pas de compte ? 
             <Link
               href="/register"
               className="font-semibold text-gray-800 hover:underline dark:text-zinc-200"
